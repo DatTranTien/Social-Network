53c53475707b92cc5a740d31317194a81ea8fd67
DatTranTien <trantiendatbk@gmail.com>
2023-07-15 21:14:44 -0700

    Initial Commit

---
+++ BeReal/BeRealApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeRealApp.swift
+//  BeReal
+//
+//  Created by Tran tien dat on 15/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct BeRealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeReal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BeReal
+//
+//  Created by Tran tien dat on 15/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
