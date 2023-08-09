a8552ef6dc2773a317c380112d4fd0f723faa1d5
TranDat <dattt@mor.com.vn>
2023-08-09 14:58:03 +0700

    RequestView screen

--- BeReal/LeftMenu/LeftMenu.swift
+++ BeReal/LeftMenu/LeftMenu.swift
@@ -13,7 +13,7 @@ struct LeftMenu: View {
             ZStack{
                 Color.black.ignoresSafeArea()
                 LeftmenuTopView()
-                FriendsView()
+                RequestView()
             }
         }
     }
